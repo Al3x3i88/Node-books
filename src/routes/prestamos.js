bbfacf8fba6deec6a6a0c94b5ddb2a744efb61d3
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-02-05 00:12:03 -0500

    agregando prestamos de libros, revisar donde esta explotando

--- src/routes/prestamos.js
+++ src/routes/prestamos.js
@@ -5,6 +5,7 @@ const { v4: uuidv4 } = require('uuid')
 
 const json_db = fs.readFileSync('src/books.json', 'utf-8')
 let fichero = JSON.parse(json_db);
+
 let arrayPrestamos = fichero.prestamos;
 const users = fichero.users;
 const books = fichero.books;
@@ -14,16 +15,51 @@ router.get('/prestamos', (req, res) => {
     console.log(users);
 })
 
-router.get('/prestamos/:id/:id', (req, res) => {
-    const bookFound = books.find(book => book.id === req.params.id);
-    const userFound = users.find(user => user.id === req.params.id);
+router.get('/prestamos/:idUser/:idBook', (req, res) => {
 
-    // if (!bookFound || !userFound)
-    //     return res.status(404).json({
-    //         message: "No se puede realiazr un prestamo",
-    //     });
-    console.log(userFound);
-    console.log(bookFound);
+    const bookFound = books.find(book => book.id === req.params.idBook);
+    const userFound = users.find(user => user.id === req.params.idUser);
+
+    let newPrestamo = {
+        id: uuidv4(),
+        idUser: req.params.idUser,
+        book: [bookFound]
+    };
+
+    if (!bookFound || !userFound) {
+        res.status(404).json({
+            message: "No se puede realiazr un prestamo",
+        });
+    }
+
+    if (arrayPrestamos.length !== 0) {
+        let libro;
+        let posicion = null;
+        for (let i = 0; i < arrayPrestamos.length; i++) {
+
+            if (arrayPrestamos[i].idUser === req.params.idUser) {
+
+                libro = arrayPrestamos[i].book.filter(book => book.id === req.params.idBook)
+                posicion = i;
+            }
+        }
+
+        if (libro.length !== 0) {
+            console.log('El libro ya existe esta prestado a este usuario');
+        } else
+
+            arrayPrestamos[posicion].book.push(bookFound);
+        console.log(arrayPrestamos);
+
+    } else {
+        arrayPrestamos.push(newPrestamo)
+    }
+
+    fichero = {...fichero, "prestamos":arrayPrestamos}
+    const json_users = JSON.stringify(fichero)
+    fs.writeFileSync('src/books.json', json_users, 'utf-8')
+    res.send('Usuario Agregado correctamente')
+
 })
 
 module.exports = router;
