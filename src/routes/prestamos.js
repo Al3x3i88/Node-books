e4ea1c8681a209dfe7ccfb761b93fa6881dfbb86
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-02-06 16:38:24 -0500

    Agregado metodo path para la devolucion de libros(Aun sin revisar)

--- src/routes/prestamos.js
+++ src/routes/prestamos.js
@@ -31,7 +31,7 @@ router.post('/prestamos/:idUser/:idBook', (req, res) => {
             message: "No se puede realizar un prestamo",
         });
     }
-console.log(arrayPrestamos.length);
+    console.log(arrayPrestamos.length);
     if (arrayPrestamos.length !== 0) {
         let libro;
         let posicion = null;
@@ -44,18 +44,17 @@ console.log(arrayPrestamos.length);
             }
         }
 
-        if (libro !== undefined && libro.length !== 0) {
+        if (libro !== undefined && libro[0].id === req.params.idBook) {
             console.log('El libro ya esta prestado a este usuario');
         }
-        else if (libro === undefined ) {
+        else if (libro === undefined) {
             arrayPrestamos.push(newPrestamo)
-            console.log(arrayPrestamos);
+
         } else
 
             arrayPrestamos[posicion].book.push(bookFound);
-        console.log(arrayPrestamos);
 
-    } else {arrayPrestamos.push(newPrestamo)}
+    } else { arrayPrestamos.push(newPrestamo) }
 
     fichero = { ...fichero, "prestamos": arrayPrestamos }
     const json_users = JSON.stringify(fichero)
@@ -63,5 +62,44 @@ console.log(arrayPrestamos.length);
     res.send('Prestamo agregado')
 
 })
+
+
+router.patch('/prestamos/:idUser/:idBook', (req, res) => {
+    const bookFound = books.find(book => book.id === req.params.idBook);
+    const userFound = users.find(user => user.id === req.params.idUser);
+    if (!bookFound || !userFound) {
+        res.status(404).json({
+            message: "Informacion incorrecta",
+        });
+    }
+    if (arrayPrestamos.length !== 0) {
+        console.log(arrayPrestamos.length);
+        let libro;
+        let posicion = null;
+        for (let i = 0; i < arrayPrestamos.length; i++) {
+            if (arrayPrestamos[i].idUser === req.params.idUser) {
+                libro = arrayPrestamos[i].book.filter(book => book.id !== req.params.idBook)
+                posicion = i;
+            }
+        }
+        console.log(libro);
+        if (libro !== undefined ) {
+            arrayPrestamos[posicion].book = libro;
+            if(arrayPrestamos[posicion].book.length ===0){
+                arrayPrestamos = arrayPrestamos.filter(prestam => prestam.idUser !== req.params.idUser);
+            }
+        } else {
+            arrayPrestamos = arrayPrestamos.filter(prestam => prestam.idUser !== req.params.idUser);
+            
+        }
+        // else{console.log('Arreglo de libros vacio');}
+    }
+    fichero = { ...fichero, "prestamos": arrayPrestamos }
+    const json_users = JSON.stringify(fichero)
+    fs.writeFileSync('src/books.json', json_users, 'utf-8')
+    res.send('libro entregado ')
+    // res.render('devoluciones');
+})
+
 
 module.exports = router;
